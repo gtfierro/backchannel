fe0e528e755958d9529a5b4f450738e21218bf51
Gabe Fierro <gtfierro225@gmail.com>
2016-04-28 19:47:28 -0700

    add lua file for tests

---
+++ routestats.lua
@@ -0,0 +1,52 @@
+sh = require "stormsh"
+sh.start()
+
+heard_reset = false
+reset_sock = storm.net.udpsocket(6666, function(msg, ip)
+    if not heard_reset then
+        heard_reset = true
+        storm.os.invokeLater(10*storm.os.SECOND, storm.os.reset)
+        cord.new(function()
+            for i=1,5 do
+                cord.await(storm.os.invokeLater, 1*storm.os.SECOND)
+                storm.net.sendto(reset_sock, "RESET", "ff02::1", 6666)
+            end
+        end)
+    end
+end)
+
+s = storm.net.udpsocket(4444, function(msg, ip)
+    print(string.format("got %s from ip %s", msg, ip))
+end)
+
+i = 0
+dest = "ipaddr here"
+storm.net.clearretrystats()
+storm.net.clearroutestats()
+function dosend() 
+    a = storm.net.retrystats()
+    b = storm.net.routestats()
+
+    res = storm.array.fromstr(a)
+    pkt_cnt = res:get_as(storm.array.UINT16, 0)
+    tx_cnt = res:get_as(storm.array.UINT16_BE, 1)
+
+    res = storm.array.fromstr(b)
+    mi_sent = res:get_as(storm.array.UINT8, 0)
+    mi_recv = res:get_as(storm.array.UINT8, 1)
+    rs_sent = res:get_as(storm.array.UINT8, 2)
+    rs_recv = res:get_as(storm.array.UINT8, 3)
+
+    print(string.format("MI sent/recv: %d/%d RS sent/reccv %d/%d", mi_sent, mi_recv, rs_sent, rs_recv))
+    pkt = {seq = i, pkt = pkt_cnt, tx = tx_cnt,  mi_sent = mi_sent, mi_recv = mi_recv, rs_sent = rs_sent, rs_recv = rs_recv}
+    rv = storm.net.sendto(s, storm.mp.pack(pkt), dest, 4444)
+    i = i + 1
+    if rv > 0 then
+        storm.net.clearretrystats()
+        storm.net.clearroutestats()
+    end
+end
+
+storm.os.invokePeriodically(10 * storm.os.SECOND, dosend)
+
+cord.enter_loop()
